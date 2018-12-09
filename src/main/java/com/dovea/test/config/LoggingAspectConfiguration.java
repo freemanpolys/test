78d204c214b5d935a252a93e529e97f19e453398
James Kokou GAGLO <jamesgaglo.kokou@orange-sonatel.com>
2018-12-09 08:16:45 +0000

    Initial application generated by JHipster-5.7.0

---
+++ src/main/java/com/dovea/test/config/LoggingAspectConfiguration.java
@@ -0,0 +1,19 @@
+package com.dovea.test.config;
+
+import com.dovea.test.aop.logging.LoggingAspect;
+
+import io.github.jhipster.config.JHipsterConstants;
+
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class LoggingAspectConfiguration {
+
+    @Bean
+    @Profile(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT)
+    public LoggingAspect loggingAspect(Environment env) {
+        return new LoggingAspect(env);
+    }
+}
---
+++ src/main/java/com/dovea/test/repository/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Data JPA repositories.
+ */
+package com.dovea.test.repository;
